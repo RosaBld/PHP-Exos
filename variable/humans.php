6732ecb3962bce32d64902e8fa69b7249b119d8c
Rosa <rosalie.boulard@gmail.com>
2023-10-19 16:35:35 +0200

    add

--- variable/humans.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head><title>Hi!</title></head>
-  <body>
-  	<?php if (isset($_GET['name'])){ ?>
-    	<h1>Aloha <?php echo $_GET['name']; ?>!</h1>
-   	<?php } ?>
-    
-  </body>
-</html>
