3e9427590e965ba6c5d5f9610eeba0d2cb9ff588
Rosa <rosalie.boulard@gmail.com>
2023-10-24 14:42:35 +0200

    adding exercices

--- fonctions/fonctions.php
+++ fonctions/fonctions.php
@@ -7,12 +7,14 @@ function capitalize($name) {
 echo capitalize('maria');
 
 echo '<br>';
+echo '<br>';
 
 function displayYear() {
     return date('Y-m-d H:i:s');
 }
 echo displayYear();
 
+echo '<br>';
 echo '<br>';
 
 function sum($a, $b) {
@@ -29,6 +31,7 @@ echo '<br>';
 echo sum ('2', '7');
 
 echo '<br>';
+echo '<br>';
 
 function acronym($string) {
     $words=explode(' ', $string);
@@ -41,6 +44,8 @@ function acronym($string) {
 echo acronym('In code we trust!');
 echo'<br>';
 echo acronym("I'm just a baby!");
+echo '<br>';
+
 echo '<br>';
 
 function replace($words) {
@@ -84,5 +89,46 @@ echo feedback("Incorrect email address", "success");
 echo feedback("Incorrect email address", "info");
 echo "</br>";
 
+echo '<br>';
+
+function generateWord($minLength, $maxLength) {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    $length = rand($minLength, $maxLength);
+    $word = '';
+    for ($i = 0; $i < $length; $i++) {
+        $index = rand(0, strlen($alphabet) - 1);
+        $word .= $alphabet[$index];
+    }
+    return $word;
+}
+
+$word1 = generateWord(1, 5);
+$word2 = generateWord(7, 15);
+
+echo "<h2>Generate a new word</h2>";
+echo "<p>$word1 $word2</p>";
+echo "<button onclick=\"location.reload()\">Generate</button>";
+
+echo '<br>';
+echo '<br>';
+
+function decapitalize($string) {
+    $string=strtolower($string);
+    $string=ucfirst($string);
+    return $string;
+}
+echo decapitalize("STOP YELLING I CAN'T HEAR MYSELF THINKING!!");
+echo '<br>';
+echo '<br>';
+
+function coneVolume($radius, $height) {
+    $volume = (1/3) * pi() * pow($radius, 2) * $height;
+    $message="The volume of the cone which ray is $radius and height is $height is: ";
+    return "$message $volume";
+}
+echo coneVolume(5, 10, "The volume of the cone is: ");
+
+echo '<br>';
+echo '<br>';
 
 ?>
